3bd3330916b33fb7e243d21ea51ca00b3a89e1a5
Castracani <willmyers993@gmail.com>
2018-06-22 19:04:42 -0400

    something in my style.css is making it so my Web page is uninteractive; I commented it out, but I'd like to have it fixed

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -13,20 +13,51 @@ var APIkey = "69ycAJq916QcNVDqkghlLHb2IrLfz4g5";
 
 });*/
 
+$(window).ready(function() {
+$("#entire-container").fadeIn(4000);
+});
 //startUp function generates the buttons from the given array//
 function startUp() {
     for (i = 0; i < subjectsArray.length; i++) {
         var subjectsIndex = subjectsArray[i];
         newButton = $("<button>" + subjectsIndex + "</button>");
-        newButton.attr( {class: "btn btn-dark work-button", value: subjectsIndex.toLowerCase()} );
+        newButton.attr({ class: "btn btn-dark work-button", value: subjectsIndex.toLowerCase() });
         newButton.attr("data-subject", subjectsIndex.toLowerCase());
         $("#buttons-column").append(newButton);
     }
 };
 
+//function for taking user-input and creating it into a button
+$("#submit-btn").on("click", function(){
+ var valuedUserInput = $("input[name='input']").val().trim();
+ console.log(valuedUserInput);
+ $("#submit-field").val('');
+ valuedUserInput = firstCap(valuedUserInput);
+ console.log(valuedUserInput);
+ subjectsArray.push(valuedUserInput);
+ console.log(subjectsArray);
+ $("#buttons-column").empty();
+ startUp();
+});
+
+//valuedUserInput.addEventListener("keyup", function(f) {
+//    f.preventDefault();
+//    if (f.keyCode === 13) {
+//        $("#submit-btn").click();
+//    }
+//});
+
+//function to take the user input and capitalize the first letter of every word
+function firstCap(str)
+{
+ return str.toString().replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + 
+ txt.substr(1).toLowerCase();});
+}
+
+
 //on-click function for the dynamically-generated buttons//
-$(document).on("click", ".work-button", function(e) {
-    
+$(document).on("click", ".work-button", function (e) {
+
     //prevents default action of ...whatever these buttons do
     e.preventDefault();
 
@@ -42,73 +73,69 @@ $(document).on("click", ".work-button", function(e) {
     //queryURL utilizing the taken value previously acquired
     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + grabbedValue + "&rating=pg&limit=10&api_key=" + APIkey;
 
-     //initiating the AJAX call
-     $.ajax({
-         url: queryURL,
-         method: "GET",
-     })
+    //initiating the AJAX call
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+    })
 
-     .then(function(response) {
-         console.log(queryURL);
-         console.log(response);
+        .then(function (response) {
+            console.log(queryURL);
+            console.log(response);
 
-         //stores the data from the AJAX call into the results variable
-         var results = response.data
-         //creating a new row
-         var output = $("<div>");
-         output.attr("class", "row ");
-         
+            //stores the data from the AJAX call into the results variable
+            var results = response.data
+            //creating a new row
+            var output = $("<div>");
+            output.attr("class", "row ");
 
-        //for loop to grab the .gifs' and populate them in the HTML page
-         for (i = 0; i < results.length; i++) {
 
-            var columns = $("<div class='col-sm-6 col-md-6 col-lg-6 d-flex align-items-center justify-content-center dynamic-gif'>");
-            output.append(columns);
-            console.log(columns);
-            var rating = $("<p>Rating: " + results[i].rating + "</p>");
-            columns.append(rating);
-            var subjectGIF = $("<img></img>");
-            subjectGIF.attr("src", results[i].images.fixed_height_still.url);
-            subjectGIF.attr("data-still", results[i].images.fixed_height_still.url);
-            subjectGIF.attr("data-animate", results[i].images.fixed_height.url);
-            subjectGIF.attr("data-state", "still");
-            subjectGIF.attr("class", "gif");
-            columns.append(subjectGIF);
-            $("#gifs-here").append(output);
-             
+            //for loop to grab the .gifs' and populate them in the HTML page
+            for (i = 0; i < results.length; i++) {
 
-           /* if((i%5)==0)
-            {
-                //when the sixth .GIF is reached, a new row is created
-                output.append("</div><div class='row'>" + "<div class='col-2 col-sm-2 col-md-2 col-lg-2 dynamic-gif'></div>");
-                $(".dynamic-gif").append("<p>testing</p>");
+                //creates a global variable with necessary class attributes in one step
+                var columns = $("<div class='col-sm-6 col-md-6 col-lg-6 d-flex align-items-center justify-content-center dynamic-gif'>");
+                //appending the new column to the output div
+                output.append(columns);
+                console.log(columns);
+                //creating a new global variable holding the rating inside of a paragraph tag
+                var rating = $("<p>Rating: " + results[i].rating + "</p>");
+                //appending this rating to the newly created column
+                columns.append(rating);
+                //creating a new global variable holding the image tag and all of the necessary data values
+                var subjectGIF = $("<img></img>");
+                subjectGIF.attr("src", results[i].images.fixed_height_still.url);
+                subjectGIF.attr("data-still", results[i].images.fixed_height_still.url);
+                subjectGIF.attr("data-animate", results[i].images.fixed_height.url);
+                subjectGIF.attr("data-state", "still");
+                subjectGIF.attr("class", "gif");
+                //appending this image tag to the columns tag, appearing after the rating element
+                columns.append(subjectGIF);
+                //appending everything to the HTML page
+                $("#gifs-here").append(output);
             }
-            else
-            {
-                //.GIF's are all given separate columns
-                output.append("<div class='col-2 col-sm-2 col-md-2 col-lg-2 dynamic-gif'></div>");
-                $(".dynamic-gif").append("<p>testing</p>");
-            }
-            if((i%5)!=0)
-            {
-                output.append("</div><div class='row'>");
-            }
-
-            $("#entire-container").append(output);*/
-
-            //HTML output
-
-
-         }
-
-
-
-
-     });
-
+        });
 });
 
+//Function for on-click events for the .gifs
+$(document).on("click", ".gif", function(f) {
+    //prevents default action(?)
+    f.preventDefault();
 
+    //grabs the value of the data-state for whichever .gif is clicked
+   var state = $(this).attr("data-state");
+
+   // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+   //Then set the image's data-state to animate
+   //Else set src to the data-still value
+   if (state === "still") {
+       $(this).attr("src", $(this).attr("data-animate"));
+       $(this).attr("data-state", "animate");
+   } else {
+       $(this).attr("src", $(this).attr("data-still"));
+       $(this).attr("data-state", "still");
+   } 
+});
 
 startUp();
 
